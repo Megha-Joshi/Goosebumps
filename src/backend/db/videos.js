29386004d70cfd72c0eceb7a9653bfc61767bd88
Megha-Joshi <megha24joshi@gmail.com>
2022-06-05 19:48:39 +0530

    feat: db-added

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,53 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "ii_YbZqHmkU",
+    title: "The 3 Most Haunted Places in Asia",
+    creator: "Travel + Leisure",
+    thumbnail: "https://i3.ytimg.com/vi/ii_YbZqHmkU/maxresdefault.jpg",
+    views: "100,454 views",
+    date: "Oct 25, 2019",
+    category: "Haunted",
+    url: "https://www.youtube.com/watch?v=ii_YbZqHmkU",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "eL85jA5HwJ8",
+    title: "7 Mysterious Places in Africa",
+    creator: "ohAFRO",
+    thumbnail: "https://i3.ytimg.com/vi/eL85jA5HwJ8/maxresdefault.jpg",
+    views: "10,219 views",
+    date: "Oct 6, 2020",
+    category: "Mysterious",
+    url: "https://www.youtube.com/watch?v=eL85jA5HwJ8",
+  },
+  {
+    _id: "nXSKMHRnrR8",
+    title: "7 Abandoned Places in Europe",
+    creator: "World Unearthed",
+    thumbnail: "https://i3.ytimg.com/vi/nXSKMHRnrR8/maxresdefault.jpg",
+    views: "3,994 views",
+    date: "Jul 29, 2021",
+    category: "Abandoned",
+    url: "https://www.youtube.com/watch?v=nXSKMHRnrR8"
+  },
+  {
+    _id: "afTaB3qkyNc",
+    title: "Haunted Places in the American South",
+    creator: "Destination Tips",
+    thumbnail: "https://i3.ytimg.com/vi/afTaB3qkyNc/maxresdefault.jpg",
+    views: "7,032 views",
+    date: "Apr 29, 2019",
+    category: "Abandoned",
+    url: "https://www.youtube.com/watch?v=afTaB3qkyNc"
+  },
+  {
+    _id: "K3D0ZKsEWY4",
+    title: "Mysterious Places in The United States",
+    creator: "World According To Briggs",
+    thumbnail: "https://i3.ytimg.com/vi/K3D0ZKsEWY4/maxresdefault.jpg",
+    views: "1,177,446 views",
+    date: "Apr 3, 2020",
+    category: "Mysterious",
+    url: "https://www.youtube.com/watch?v=K3D0ZKsEWY4",
   },
 ];
