3bb689ec557cf9a79c1a81bddf8d2d5f3a6a69a1
Megha-Joshi <megha24joshi@gmail.com>
2022-06-26 16:17:22 +0530

    feat: playlist-modal-and-button-added

--- src/pages/Homepage/homepage.jsx
+++ src/pages/Homepage/homepage.jsx
@@ -3,18 +3,23 @@ import { Navbar } from "../../video-components/Navbar/navbar";
 import { Sidebar } from "../../video-components/Sidebar/sidebar";
 import "../../root.css"
 import "../Homepage/homepage.css";
+import "../Login-Signup/auth.css";
 import { useVideo } from "../../context/videoContext";
 import { useAuth } from "../../context/authContext";
 import { useFilter } from "../../context/filterContext";
 import { NavLink } from "react-router-dom";
+import Modal from 'react-modal';
 
 const Homepage = () => {
 const [sidebar, setSideBar] = useState(true);
 const { videoState, addVideoToHistory, addItemToLikedVideos, removeItemFromLikedVideos, addItemToWatchLaterVideos,
-removeItemFromWatchLaterVideos } = useVideo();
+removeItemFromWatchLaterVideos, addNewPlaylist, addVideoToPlaylist, removeVideoFromPlaylist } = useVideo();
 const { videos, categories } = videoState;
 const { token } = useAuth();
 const { filterState, filterDispatch, filteredVideos } = useFilter();
+const [ modal, setModal] = useState(false);
+const [playlistName, setPlaylistName ] = useState("");
+const [currVideoState, setCurrVideoState ] = useState("");
 
 const likeHandler = (token, video) => {
 videoState.likedVideos.some((item) => item._id === video._id) ?
@@ -26,6 +31,38 @@ const watchLaterHandler = ( token, video) => {
 videoState.watchLater.some((item) => item._id === video._id) ?
 removeItemFromWatchLaterVideos(video._id, token) : addItemToWatchLaterVideos(token,video)
 }
+
+const playListHandler = ( token, video ) => {
+    if(token){
+        setModal(true);
+        var currVideo = videoState.videos.find((item) => item._id === video._id); 
+        setCurrVideoState(currVideo);
+    }
+}
+
+const isVideoExistInPlaylist = (playlist) => playlist.videos.some((video) => video._id === currVideoState._id);
+
+const checkVideoInPlaylistHandler = (currVideoState, playlist) => {
+    isVideoExistInPlaylist(playlist) ? removeVideoFromPlaylist(token, currVideoState._id, playlist._id) : addVideoToPlaylist(token, currVideoState, playlist._id);
+   }
+
+const  makeNewPlaylistHandler = (playlistName) => {
+    playlistName && addNewPlaylist(token, playlistName);
+    setPlaylistName("");
+  }
+
+const customStyle = {
+    overlay: {
+      top: "6rem",
+      backgroundColor: "rgba(52, 58, 64, 0.8)",
+    },
+    content: {
+      width: "18rem",
+      height: "20rem",
+      margin: "5rem auto",
+      backgroundColor: "var(--accent-color)",
+    },
+  };
 
 return (
 <div className="App">
@@ -52,7 +89,7 @@ return (
                     <div className="card-footer">
                         <p className="card-content">{video.views}</p>
                         <p className="card-content">{video.date}</p>
-                        <li className="modal-list"><span className="card-icon"><i
+                        <li className="modal-list" onClick={() => playListHandler(token, video)}><span className="card-icon"><i
                                     class="fad fa-list"></i></span>
                         </li>
                         <li className="modal-list" onClick={()=> likeHandler(token,video)}><span
@@ -65,11 +102,41 @@ return (
                                     video._id)? "fas fa-clock" : "far fa-clock" }></i></span>
                         </li>
                     </div>
-
                 </article>
                 ))}
             </div>
         </section>
+
+        {
+            modal && (
+                <Modal isOpen={ modal } style={ customStyle }>
+                    <header className="modal-header">
+                        <h3 className="card-title">Create New Playlist</h3>
+                        <i class="fas fa-times card-icon" onClick={() => setModal(false)}></i>
+                    </header>
+                    <section>
+                        {
+                            videoState.playlists.length > 0 && 
+                            videoState.playlists.map((playlist) => {
+                                return(
+                                    <div key={playlist._id}>
+                                        <input type = "checkbox" checked={isVideoExistInPlaylist(playlist)} 
+                                        onChange={() => checkVideoInPlaylistHandler(currVideoState, playlist)}/>
+                                        <span className="card-content">{playlist.title}</span>
+                                    </div>
+                                );
+                            })
+                        }
+                    </section>
+                    <main className="modal-subhead">
+                        <label className="card-subhead">Name</label>
+                        <input type="text" value={playlistName}
+                        onChange={(e) => setPlaylistName(e.target.value)}/>
+                    </main>
+                    <button className= "login-btn center-btn" onClick={() => makeNewPlaylistHandler(playlistName)}>Create</button>
+                </Modal>
+            )
+        }
     </main>
 </div>
 );
